6658f4ca31b2e87d6630ac3bac2ff9b9ead4542b
= <webdev.jameslemire@gmail.com>
2018-01-29 13:29:57 -0700

    JL

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,12 @@ set -x
 cd ~/docker-test
 git pull
 echo pulled
+cd ~/docker-test/public
+npm install
+npm run build
 cd ~/docker-test
 docker build -t test .
-docker image prune -f
 docker stop test_container
 docker rm test_container
 docker run -d -p 3000:3000 --name="test_container" -i test
+docker image prune -f
