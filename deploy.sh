0031623e30bb7be6b77587f3d026d58fb1dc3593
= <webdev.jameslemire@gmail.com>
2018-01-29 14:18:05 -0700

    JL

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ cd ~/docker-test/public
 yarn
 yarn build
 cd ~/docker-test
-docker build -t test --squash .
+docker build -t test .
 docker stop test_container
 docker rm test_container
 docker run -d -p 3000:3000 --name="test_container" -i test
