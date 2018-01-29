f8335006e6bca9bfd193eadcadcf6b6d5a18efcc
= <webdev.jameslemire@gmail.com>
2018-01-29 13:25:36 -0700

    JL

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ git pull
 echo pulled
 cd ~/docker-test
 docker build -t test .
+docker image prune -f
 docker stop test_container
 docker rm test_container
 docker run -d -p 3000:3000 --name="test_container" -i test
