2f66faab97f8c7aed0a97e260fd38d1dbfe6cd08
= <webdev.jameslemire@gmail.com>
2018-01-29 10:21:34 -0700

    JL master

--- public/src/__tests__/math.test.js
+++ public/src/__tests__/math.test.js
@@ -2,4 +2,8 @@ describe('Math Functions', () => {
   it('should add two numbers together', () => {
     expect( 2 + 2 ).toEqual( 4 );
   });
+
+  it('should subtract two numbers together', () => {
+    expect( 2 - 2 ).toEqual( 4 );
+  });
 });
