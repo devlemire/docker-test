caaf415ae8ff62c76b2cc691d4291867a386de98
= <webdev.jameslemire@gmail.com>
2018-01-29 13:01:20 -0700

    JL

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,8 @@ cd ~/docker-test
 git pull
 echo pulled
 cd ~/docker-test
+npm install
+echo installed server depedencies
+cd ~/docker-test
 forever start -l test.log -a --uid test index.js
 echo server started
