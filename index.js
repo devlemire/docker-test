7a9feedfa2224262f27d67fa54659da9e87d91e6
= <webdev.jameslemire@gmail.com>
2018-01-30 14:31:45 -0700

    JL docker-compose - SO MUCH KNOWLEDGE.

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const Path = require('path');
+const cors = require('cors');
+const massive = require('massive');
 
 const app = express();
+
+massive({
+  host: 'postgres',
+  port: 5432,
+  database: 'docker_db',
+  user: 'james',
+  password: 'swag1234'
+}).then( db => {
+  console.log('@@@@@@ DB SUCCESSFULLY CONNECTED @@@@@@');
+  app.set('db', db);
+  db.run('SELECT COUNT(*) FROM cars').then( data => console.log( data ) );
+  db.run('SELECT COUNT(*) FROM trucks').then( data => console.log( data ) );
+}).catch( err => console.error( err ) );
+
+app.use( cors() );
+
+app.get('/api/cars', ( req, res, next ) => {
+  const db = app.get('db');
+  db.run('SELECT COUNT(*) FROM cars').then( data => {
+    res.status(200).send( data[0] );
+  });
+});
+
+app.get('/api/trucks', ( req, res, next ) => {
+  const db = app.get('db');
+  db.run('SELECT COUNT(*) FROM trucks').then( data => {
+    res.status(200).send( data[0] );
+  });
+});
 
 app.use( express.static( __dirname + '/build/' ) );
 app.get( '*', ( req, res, next ) => {
   res.sendFile( Path.resolve( __dirname + '/build/index.html' ) );
 });
 
-app.listen( 3000, () => console.log('Server listening on port 3000') );
+app.listen( 3001, () => console.log('Server listening on port 3000') );
--- public/src/App.js
+++ public/src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import axios from 'axios';
+
+
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      carsCount: 0,
+      trucksCount: 0
+    };
+  }
+
+  componentDidMount() {
+    axios.all([
+      axios.get('http://localhost:3001/api/cars'),
+      axios.get('http://localhost:3001/api/trucks')
+    ]).then( results => {
+      this.setState({ carsCount: results[0].data.count, trucksCount: results[1].data.count });
+    });
+  }
+
   render() {
+    console.log( this.state );
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to Burrito</h1>
+          <h1 className="App-title">Welcome to taco taco!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <h1>Cars Count: { this.state.carsCount }</h1>
+        <h1>Trucks Count: { this.state.trucksCount }</h1>
       </div>
     );
   }
